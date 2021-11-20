1b297fe087b7da0c499beb5b0c49ad80f3ee382c
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-11-20 19:08:51 +0200

    ✨: add Entity

---
+++ HunterModel/Entity.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace HunterModel
+{
+    public class Entity
+    {
+        public float BodyRadius { get; }
+        public float MoveSpeed { get; }
+        public Vector2 Position { get; }
+
+        public void Move() { }
+        public void Die() { }
+    }
+}
