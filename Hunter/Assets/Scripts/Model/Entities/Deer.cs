92f3c1994174a6fe66a39adccb7a123b1ac4c822
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-24 13:38:15 +0200

    :sparkles:: update class Deer

--- Hunter/Assets/Scripts/Model/Entities/Deer.cs
+++ Hunter/Assets/Scripts/Model/Entities/Deer.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Hunter.Model.Entities
 {
-    class Deer
+    class Deer : Animal
     {
+        public float RunSpeed { get; set; }
+
+        public Deer() : base()
+        {
+            MaxSpeed = 1f * 0.001f;
+        }
+
+        public override void Move()
+        {
+            Vector2 steering = Behaviours.Wander(this);
+            Velocity = Vector2.Multiply(Velocity + steering, MaxSpeed);
+            Position += Velocity;
+        }
+
     }
 }
