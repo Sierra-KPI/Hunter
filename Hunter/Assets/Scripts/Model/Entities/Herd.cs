88478d8927b237711046e3b1ed648da8efb70921
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-24 13:40:36 +0200

    :sparkles:: update class Herd

--- Hunter/Assets/Scripts/Model/Entities/Herd.cs
+++ Hunter/Assets/Scripts/Model/Entities/Herd.cs
@@ -3,7 +3,28 @@
 
 namespace Hunter.Model.Entities
 {
-    class Herd
+    class Herd : Animal
     {
+        public Deer[] Deers;
+
+        public Herd()
+        {
+            int numberOfDeers = new Random().Next(3, 10);
+            Deers = new Deer[numberOfDeers];
+            for (var i = 0; i < numberOfDeers; i++)
+            {
+                Deers[i] = new Deer();
+            }
+
+        }
+
+        public override void Move()
+        {
+            foreach (var deer in Deers)
+            {
+                deer.Move();
+            }
+        }
+
     }
 }
