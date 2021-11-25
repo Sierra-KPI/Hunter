5c9b5014002719c9e545f6f14b719c0d16a97314
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-11-25 15:03:34 +0200

    :sparkles:: add class HerdAnimal

---
+++ Hunter/Assets/Scripts/Model/Entities/HerdAnimal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter.Model.Entities
+{
+    public abstract class HerdAnimal : Animal
+    {
+
+
+        public override void Move() {}
+        public abstract void MoveInHerd(Animal[] animals);
+
+    }
+}
