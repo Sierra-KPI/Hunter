3560a3c6a124a5c2267332dd296b29591a508f93
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-03 15:53:08 +0200

    :sparkles:: add static method CreateAnimals to Rabbit

--- Hunter/Assets/Scripts/Model/Entities/Rabbit.cs
+++ Hunter/Assets/Scripts/Model/Entities/Rabbit.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Numerics;
 using Hunter.Model.Behaviours;
 
@@ -9,13 +11,32 @@ namespace Hunter.Model.Entities
 
         public Rabbit() : base()
         {
-            MaxSpeed = 2 * 0.001f
+            MaxSpeed = 2 * 0.001f;
             BodyRadius = 0.25f; // If size in unity = 3,
                                 // then divide it by 12 and
                                 // You'll get BodyRadius
             WanderCircleDistance = 10;
             WanderCircleRadius = 4;
             MaxWanderShift = 3;
+        }
+
+        public static List<Animal> CreateAnimals(int numberOfRabbits)
+        {
+            var rabbits = new List<Animal>();
+            for (var i = 0; i < numberOfRabbits; i++)
+            {
+                int xPos = new Random().Next(-3, 4);
+                int yPos = new Random().Next(-3, 4);
+
+                Rabbit rabbit = new Rabbit
+                {
+                    //Position = new Vector2(xPos, yPos)
+                    Position = Vector2.Zero
+                };
+
+                rabbits.Add(rabbit);
+            }
+            return rabbits;
         }
 
         public override void Move()
