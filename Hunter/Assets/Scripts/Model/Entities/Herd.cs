5a6c9be8fa85c985d81839540a1b5c8edd43372a
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-03 15:52:00 +0200

    :sparkles:: add static method CreateAnimals to Herd

--- Hunter/Assets/Scripts/Model/Entities/Herd.cs
+++ Hunter/Assets/Scripts/Model/Entities/Herd.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace Hunter.Model.Entities
 {
@@ -16,6 +17,25 @@ namespace Hunter.Model.Entities
                 _animals[i] = new Deer();
             }
 
+        }
+
+        public static List<Animal> CreateAnimals(int numberOfHerds)
+        {
+            var herds = new List<Animal>();
+            for (var i = 0; i < numberOfHerds; i++)
+            {
+                int xPos = new Random().Next(-3, 4);
+                int yPos = new Random().Next(-3, 4);
+
+                Herd herd = new Herd
+                {
+                    //Position = new Vector2(xPos, yPos)
+                    Position = Vector2.Zero
+                };
+
+                herds.Add(herd);
+            }
+            return herds;
         }
 
         public override void Move()
