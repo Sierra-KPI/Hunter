f50cf1dc4a129770a39de37af618e3e115ae660e
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-03 15:54:03 +0200

    :recycle:: rewrite HunterGame

--- Hunter/Assets/Scripts/Model/HunterGame/HunterGame.cs
+++ Hunter/Assets/Scripts/Model/HunterGame/HunterGame.cs
@@ -1,57 +1,29 @@
 using System;
 using System.Collections.Generic;
-using System.Numerics;
 using Hunter.Model.Entities;
 
 namespace Hunter.Model.HunterGame
 {
     public class HunterGame
     {
-        public List<Rabbit> Rabbits = new();
-        public List<Herd> HerdsOfDeer = new();
+        public Dictionary<AnimalType, List<Animal>> Animals = new();
 
         public HunterGame(int rabbits, int deers, int wolfs)
         {
-            for (var i = 0; i < rabbits; i++)
-            {
-                int xPos = new Random().Next(-3, 4);
-                int yPos = new Random().Next(-3, 4);
+            Animals.Add(AnimalType.Rabbit, Rabbit.CreateAnimals(rabbits));
+            Animals.Add(AnimalType.Deer, Herd.CreateAnimals(deers));
 
-                Rabbit rabbit = new Rabbit
-                {
-                    //Position = new Vector2(xPos, yPos)
-                    Position = Vector2.Zero
-                };
-
-                Rabbits.Add(rabbit);
-            }
-
-            for (var i = 0; i < deers; i++)
-            {
-                int xPos = new Random().Next(-3, 3);
-                int yPos = new Random().Next(-3, 3);
-
-                Herd herd = new Herd
-                {
-                    Position = new Vector2(xPos, yPos)
-                    //Position = Vector2.Zero
-                };
-
-                HerdsOfDeer.Add(herd);
-            }
         }
-    
+
         public void Update()
         {
-            foreach (Rabbit rabbit in Rabbits)
+            foreach (AnimalType animalType in (AnimalType[])Enum.GetValues(typeof(AnimalType)))
             {
-                rabbit.Move();
+                foreach (Animal animal in Animals[animalType])
+                {
+                    animal.Move();
+                }
             }
-            foreach (Herd herd in HerdsOfDeer)
-            {
-                herd.Move();
-            }
-
         }
     }
 }
