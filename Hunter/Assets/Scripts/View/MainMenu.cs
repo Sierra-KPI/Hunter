2b2a33cd1b618792c3c4ccf3cc4873068fc28a64
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-05 14:31:28 +0200

    :sparkles:: add UI for menu

---
+++ Hunter/Assets/Scripts/View/MainMenu.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    private Slider[] _sliders;
+    private Button _playButton;
+    private Button _quitButton;
+
+    public void Start()
+    {
+        _sliders = FindObjectsOfType<Slider>();
+        foreach (var slider in _sliders)
+        {
+            slider.onValueChanged.AddListener(delegate { OnSliderChanged(slider); });
+        }
+
+        _playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+        _playButton.onClick.AddListener(delegate { OnPlayButtonClick(); });
+
+        _quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+        _quitButton.onClick.AddListener(delegate { OnQuitButtonClick(); });
+    }
+
+    public void OnSliderChanged(Slider slider)
+    {
+        Text text = slider.GetComponentInChildren<Text>();
+        text.text = slider.value.ToString();
+    }
+
+    public void OnPlayButtonClick()
+    {
+        foreach (var slider in _sliders)
+        {
+            PlayerPrefs.SetInt(slider.name, (int)slider.value);
+        }
+        SceneManager.LoadScene("MainScene");
+    }
+
+
+    public void OnQuitButtonClick()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
