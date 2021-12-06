98ffed6c35fc3df24917678f3695c21656d011f0
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-06 15:02:55 +0200

    :sparkles:: add MousePosition

--- Hunter/Assets/Scripts/Controller/Controller.cs
+++ Hunter/Assets/Scripts/Controller/Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 public class Controller : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class Controller : MonoBehaviour
     {
 
         HunterControler();
+        MousePosition();
     }
 
 
@@ -29,6 +31,17 @@ public class Controller : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("Hunter Move Right");
+        }
+    }
+
+
+    private void MousePosition()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+            Debug.Log(worldPosition.x + " " + worldPosition.y);
         }
     }
 
