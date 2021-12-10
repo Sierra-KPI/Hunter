354282cad181c1c4a750acdf0a760671477aa391
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-10 21:04:22 +0200

    ✨: update border

--- Hunter/Assets/Scripts/Model/Behaviours/AvoidBordersBehaviour.cs
+++ Hunter/Assets/Scripts/Model/Behaviours/AvoidBordersBehaviour.cs
@@ -5,7 +5,7 @@ namespace Hunter.Model.Behaviours
 {
     public static class AvoidBordersBehaviour
     {
-        private static readonly float s_safeBorder = 12f; // CHANGE
+        private static readonly float s_safeBorder = 7.2f; // CHANGE
 
         public static Vector2 AvoidBorders(Animal animal)
         {
