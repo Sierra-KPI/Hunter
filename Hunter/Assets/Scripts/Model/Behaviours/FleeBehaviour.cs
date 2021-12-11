1ffb41eee109b65bab0791b848070e3b51584a5e
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-11 14:44:15 +0200

    :fire:: remove method

--- Hunter/Assets/Scripts/Model/Behaviours/FleeBehaviour.cs
+++ Hunter/Assets/Scripts/Model/Behaviours/FleeBehaviour.cs
@@ -8,37 +8,6 @@ namespace Hunter.Model.Behaviours
     {
         public static Vector2 RunAway(Animal animal)
         {
-            Vector2 GetFleeTargetPosition()
-            {
-                List<Entity> target = new List<Entity>();
-                float minDistance = float.MaxValue;
-                Vector2 _targetPosition = animal.Position;
-
-                if (animal.Entities.Count != 0)
-                {
-                    target.Add(animal.Entities[0]);
-
-                    foreach (Entity entity in animal.Entities)
-                    {
-                        target[0] = entity;
-
-                        float targetDistance = Vector2.Distance(animal.Position,
-                            target[0].Position);
-
-                        if (targetDistance < minDistance)
-                        {
-                            minDistance = targetDistance;
-                            _targetPosition = entity.Position;
-                        }
-                        else continue;
-                    }
-                }
-                else
-                {
-                    _targetPosition = Vector2.Zero;
-                }
-                return _targetPosition;
-            }
             Vector2 targetPosition = PursueBehaviour.Chase(animal);
             Vector2 desiredVelocity = Vector2.Multiply(-targetPosition + animal.Position, animal.MaxSpeed);
             return desiredVelocity;
